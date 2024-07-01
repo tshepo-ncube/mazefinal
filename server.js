132f0a3eb372de4c67c6aa37b388483679eef00a
tshepo-ncube <115148619+tshepo-ncube@users.noreply.github.com>
2024-07-01 16:03:31 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const initializeApp = require("firebase/app");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg + " tshepo");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
